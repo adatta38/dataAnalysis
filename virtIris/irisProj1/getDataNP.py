c55879dab61cde034976d8c6d84f84eb368ce62d
ABHISHEK DATTA <abhishekdatta@ABHISHEKs-Mac-mini.local>
2018-08-22 19:54:08 +0530

    Second Commit

--- virtIris/irisProj1/getDataNP.py
+++ virtIris/irisProj1/getDataNP.py
@@ -1,7 +1,11 @@
-import numpy as np
+import pandas as pd
+# import numpy as np
 
-filename = 'iris.data'
+# filename = 'iris.data'
 
-data = np.loadtxt(filename, delimiter=',')
+data = pd.read_csv("iris.data")
+# data = pd.read_csv("/Users/abhishekdatta/Documents/ABHISHEK DATTA/LEARNINGS/PROJECTS/AI/ML/dataAnalysis/virtIris/irisProj1/iris.data")
+# data = np.loadtxt(filename, delimiter=',')
+
 
 print(data)
